af20daf9eddef65d201e878b9b8f61c06e4533df
Hafeez Uddin Ahmed Siddiqui <hafeez27isbest@gmail.com>
2024-09-08 03:13:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 window.onload = function () {
-    // const skillsContainer = document.getElementById("skills-container");
-    // const addSkillBtn = document.getElementById("add-skill-btn");
-    // const skillsList = document.getElementById("skills-list");
+  
     var addMoreExperienceBtn = document.getElementById("addmore");
     addMoreExperienceBtn.addEventListener("click", addExperience);
     function addExperience() {
@@ -19,13 +17,6 @@ function addnewskill() {
     var moreskill = document.createElement("input");
     moreskill.placeholder = "Add More Skill";
     var skillContainer = document.getElementById("skill-container");
-    skillContainer.appendChild(moreskill); // Append the moreskill input element
+    skillContainer.appendChild(moreskill); 
 }
-// const addMoreskillBtn = document.getElementById("addmoreskill");
-//   addMoreskillBtn.addEventListener("click", addnewskill);
-//   function addnewskill() {
-//     const  moreskill = document.createElement("input");
-//     moreskill.placeholder = "Add More Skill";
-//     const skillContainer = document.getElementById("skill-container");
-//     skillContainer.appendChild(addMoreskillBtn);
-//   }
+
