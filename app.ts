f7b7f5b2e9df38d8fb82259f28f49b279c2d80d5
Hafeez Uddin Ahmed Siddiqui <hafeez27isbest@gmail.com>
2024-09-08 05:30:46 +0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,28 +1,42 @@
 window.onload = function () {
-
-
-  const addMoreExperienceBtn = document.getElementById("addmore");
+  const addMoreExperienceBtn = document.getElementById("addmore") as HTMLButtonElement;
 
   addMoreExperienceBtn.addEventListener("click", addExperience);
 
   function addExperience() {
     const newExperienceTextarea = document.createElement("textarea");
     newExperienceTextarea.placeholder = "Add More Experience";
-    newExperienceTextarea.rows = 5
-    newExperienceTextarea.cols = 60
+    newExperienceTextarea.rows = 5;
+    newExperienceTextarea.cols = 60;
 
-    const experienceContainer = document.getElementById("experience-container");
+    const experienceContainer = document.getElementById("experience-container") as HTMLDivElement;
     experienceContainer.appendChild(newExperienceTextarea);
   }
-}
-const addMoreskillBtn = document.getElementById("addmoreskill");
 
-addMoreskillBtn.addEventListener("click", addnewskill);
+  const addMoreskillBtn = document.getElementById("addmoreskill") as HTMLButtonElement;
 
-function addnewskill() {
-  const moreskill = document.createElement("input");
-  moreskill.placeholder = "Add More Skill";
+  addMoreskillBtn.addEventListener("click", addnewskill);
 
-  const skillContainer = document.getElementById("skill-container");
-  skillContainer.appendChild(moreskill); // Append the moreskill input element
+  function addnewskill() {
+    const moreskill = document.createElement("input");
+    moreskill.placeholder = "Add More Skill";
+    moreskill.type = "text";
+
+    const skillContainer = document.getElementById("skill-container") as HTMLDivElement;
+    skillContainer.appendChild(moreskill);
+  }
+
+  const addMoreEducationBtn = document.getElementById("addmoreeducation") as HTMLButtonElement;
+
+  addMoreEducationBtn.addEventListener("click", addEducation);
+
+  function addEducation() {
+    const newEducationTextarea = document.createElement("textarea");
+    newEducationTextarea.placeholder = "Add More Education";
+    newEducationTextarea.rows = 5;
+    newEducationTextarea.cols = 60;
+
+    const educationContainer = document.getElementById("education-container") as HTMLDivElement;
+    educationContainer.appendChild(newEducationTextarea);
+  }
 }
